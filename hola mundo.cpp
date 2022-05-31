3d34b0d07db1fa04e348abce874f2e8a08a58e15
Desconocido502 <3018554470101@ingenieria.usac.edu.gt>
2022-05-31 14:01:45 -0600

    Se creo el primer programa en c++

---
+++ hola mundo.cpp
@@ -0,0 +1,12 @@
+//Primer programa en C++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"Hola mundo :)"<<endl; //va a imprimir todo lo que escriba dentro de el
+	
+	return 0;
+}
